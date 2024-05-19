c902c42e7337cbf5c6ba8d1d8fecf21f46ad7ebe
Ga-Zampieri <gabriel_zampieri@hotmail.com>
2024-05-19 18:10:25 -0300

    java 17

--- src/main/java/com/zprmts/tcc/ecommerce/configuration/ApplicationConfiguration.java
+++ src/main/java/com/zprmts/tcc/ecommerce/configuration/ApplicationConfiguration.java
@@ -1,23 +1,12 @@
 package com.zprmts.tcc.ecommerce.configuration;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfiguration {
 
-    @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper mapper = new ModelMapper();
-        mapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.STRICT);
-        return mapper;
-    }
 
     @Bean
     public SpelAwareProxyProjectionFactory projectionFactory() {
