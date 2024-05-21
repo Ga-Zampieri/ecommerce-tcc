3f91c732d7148cecf0630ba3eaec6e21d1a2e90f
Ga-Zampieri <gabriel_zampieri@hotmail.com>
2024-05-21 11:46:34 -0300

    fix

---
+++ target/classes/db/migration/V3__Add_products.sql
@@ -0,0 +1,13 @@
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (1, 'Fragrância clássica e sofisticada para homens.', 'Hugo Boss', 21, 'Masculino, Elegante', null);
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (2, 'Perfume atemporal com notas frescas e amadeiradas.', 'Burberry', 27, 'Unissex, Clássico', null);
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (3, 'Aroma luxuoso e distinto para ocasiões especiais.', 'Bvlgari', 36, 'Masculino, Luxo', null);
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (4, 'Fragrância moderna e sensual para o dia a dia.', 'Calvin Klein', 21, 'Unissex, Casual', null);
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (5, 'Perfume marcante e elegante para mulheres.', 'Carolina Herrera', 76, 'Feminino, Elegante', null);
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (6, 'O icônico e eterno Chanel N5, símbolo de luxo.', 'Chanel N5', 192, 'Feminino, Clássico, Luxo', null);
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (7, 'Fragrância sofisticada e envolvente para mulheres.', 'Dior', 90, 'Feminino, Sofisticado', null);
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (8, 'Aroma elegante e refinado para todas as ocasiões.', 'Givenchy', 49, 'Unissex, Refinado', null);
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (9, 'Perfume ousado e moderno para homens.', 'Gucci', 44.5, 'Masculino, Moderno', null);
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (10, 'Fragrância exclusiva e luxuosa para homens.', 'Hermes', 123, 'Masculino, Luxo', null);
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (11, 'Aroma provocante e único para ocasiões especiais.', 'Jean Paul Gaultier', 12, 'Unissex, Provocante', null);
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (12, 'Perfume sofisticado e exclusivo para homens.', 'Tom Ford', 95, 'Masculino, Exclusivo', null);
+INSERT INTO perfume (id_perfume, description, name, price, categories, foto_id) VALUES (13, 'Fragrância vibrante e moderna para mulheres.', 'Versace', 37, 'Feminino, Moderno', null);
---
+++ target/classes/db/migration/V4__Add_orders.sql
@@ -0,0 +1,22 @@
+INSERT INTO orders (id, id_user, date, total_price)
+    VALUES (1, 1, '2021-04-07', 840);
+INSERT INTO orders (id, id_user, date, total_price)
+    VALUES (2, 1, '2021-04-07', 240);
+INSERT INTO orders (id, id_user, date, total_price)
+    VALUES (3, 1, '2021-04-07', 163);
+INSERT INTO orders (id, id_user, date, total_price)
+    VALUES (4, 1, '2021-04-07', 780);
+INSERT INTO orders (id, id_user, date, total_price)
+    VALUES (5, 1, '2021-04-07', 196);
+
+INSERT INTO order_item (id_order, quantity, id_perfume) VALUES (1, 2, 1);
+INSERT INTO order_item (id_order, quantity, id_perfume) VALUES (2, 3, 2);
+INSERT INTO order_item (id_order, quantity, id_perfume) VALUES (3, 2, 3);
+INSERT INTO order_item (id_order, quantity, id_perfume) VALUES (4, 1, 4);
+INSERT INTO order_item (id_order, quantity, id_perfume) VALUES (5, 1, 5);
+INSERT INTO order_item (id_order, quantity, id_perfume) VALUES (6, 1, 6);
+INSERT INTO order_item (id_order, quantity, id_perfume) VALUES (7, 1, 7);
+INSERT INTO order_item (id_order, quantity, id_perfume) VALUES (8, 2, 8);
+INSERT INTO order_item (id_order, quantity, id_perfume) VALUES (9, 4, 9);
+INSERT INTO order_item (id_order, quantity, id_perfume) VALUES (10, 2, 10);
+INSERT INTO order_item (id_order, quantity, id_perfume) VALUES (11, 2, 12);
